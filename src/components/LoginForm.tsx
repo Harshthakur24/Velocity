98758d2ae336ad0472de5d800bc47c74cca415f1
Harsh <thakur2004harsh@gmail.com>
2024-09-01 17:22:34 +0530

    Updated

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -12,24 +12,24 @@ const LoginForm = () => {
       className="flex justify-center items-center min-h-screen bg-cover bg-center"
       style={{
         backgroundImage:
-          "linear-gradient(to right, #0f172a, #1e293b, #334155, #475569, #64748b), url('https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')",
+          "linear-gradient(to right, #1e2a38, #2c3e50, #34495e, #3b4a6d, #2a3a5d), url('https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')",
       }}
     >
       <form
         action={doSocialLogin}
         className="bg-gray-900 p-10 rounded-3xl shadow-2xl w-full max-w-md animate-fadeIn"
       >
-        <h2 className="text-4xl font-bold mb-8 text-center text-white">
-          Welcome Back <MdLogin className="inline-block text-yellow-500" />
+        <h2 className="text-3xl font-bold mb-8 text-center text-gray-100">
+          Welcome to Velocity <MdLogin className="inline-block text-blue-400" />
         </h2>
         <p className="text-center text-gray-300 mb-6">
           Sign in to continue{" "}
-          <RiLockPasswordLine className="inline-block text-yellow-500" />
+          <RiLockPasswordLine className="inline-block text-blue-400" />
         </p>
         <div className="flex flex-col items-center space-y-4">
           <Button
             variant="outline"
-            className="flex items-center justify-center hover:bg-blue-600 py-2 px-4 rounded-md w-full transition duration-300 text-white"
+            className="flex items-center justify-center hover:bg-blue-700 py-2 px-4 rounded-md w-full transition duration-300 text-gray-100 border border-blue-500"
             type="submit"
             name="action"
             value="google"
@@ -39,7 +39,7 @@ const LoginForm = () => {
           </Button>
           <Button
             variant="outline"
-            className="flex items-center justify-center hover:bg-rose-800 py-2 px-4 rounded-md w-full transition duration-300 text-white"
+            className="flex items-center justify-center hover:bg-purple-700 py-2 px-4 rounded-md w-full transition duration-300 text-gray-100 border border-purple-500"
             type="submit"
             name="action"
             value="github"
@@ -49,7 +49,7 @@ const LoginForm = () => {
           </Button>
           <Button
             variant="outline"
-            className="flex items-center justify-center hover:bg-green-700 py-2 px-4 rounded-md w-full transition duration-300 text-white"
+            className="flex items-center justify-center hover:bg-green-800 py-2 px-4 rounded-md w-full transition duration-300 text-gray-100 border border-green-600"
             type="submit"
             name="action"
             value="spotify"
@@ -59,11 +59,12 @@ const LoginForm = () => {
           </Button>
         </div>
         <div className="mt-8 text-center text-gray-300">
-          <p>
-            We only support Google, GitHub, and Spotify for now.
-            <RiLockPasswordLine className="inline-block text-yellow-500" />
-            <a href="#" className="text-blue-500 hover:underline"></a>
+          <p className="mb-2">
+            We only support Google for now.<span> </span>
+            <RiLockPasswordLine className="inline-block text-blue-400" />
+            <a href="#" className="text-blue-300 hover:underline"></a>
           </p>
+          <div className="-mb-4">Made with ❤️ by Harsh</div>
         </div>
       </form>
     </div>
