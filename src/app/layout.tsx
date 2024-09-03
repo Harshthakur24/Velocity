8400d44b90977fb0702c957458be2341212c5db7
Harsh <thakur2004harsh@gmail.com>
2024-09-03 18:31:38 +0530

    Updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo.png" />
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
       </head>
